a55853d3b88a0f8bafce78d5f0c78dd668fb45ba
zenista <zenista@rocketmail.com>
2015-03-19 20:53:08 +0530

    Fixed: Quote marks (from department.csv) got included in sku display fields as well exported cols.

--- src/ericools/LincScan/MyStockItem.java
+++ src/ericools/LincScan/MyStockItem.java
@@ -341,9 +341,10 @@ public class MyStockItem {
     // get a special version of sku
     // with prefix and suffixed applied (if any)
     public String getSkuPrefixedAndSuffixed() {
-        return getStockItemValue(MyStockItem.ENTRY_PREFIX) + 
-            getStockItemValue(MyStockItem.ENTRY_SKU) +
-            getStockItemValue(MyStockItem.ENTRY_SUFFIX);
+        String prefix = getStockItemValue(MyStockItem.ENTRY_PREFIX).replaceAll("\"","");
+        String suffix = getStockItemValue(MyStockItem.ENTRY_SUFFIX).replaceAll("\"",""); 
+        String sku = getStockItemValue(MyStockItem.ENTRY_SKU);
+        return  prefix + sku + suffix;
     }
 
 }
